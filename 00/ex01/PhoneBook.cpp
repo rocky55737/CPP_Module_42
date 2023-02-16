a4bdc450f87f7be37caae5305c500250e93d1780
rocky55737 <rocky55737@gmail.com>
2023-02-16 12:21:25 +0900

    add

--- 00/ex01/PhoneBook.cpp
+++ 00/ex01/PhoneBook.cpp
@@ -1,6 +1,30 @@
 #include "PhoneBook.hpp"
 #include <iostream>
 
+void	PhoneBook::pb_init()
+{
+	string	cmd;
+
+	cmd = "started";
+	while (cmd == "EXIT")
+	{
+		std::cout << "1. ADD   2. SEARCH   3. EXIT" << std::endl;
+		std::getline(std::cin, cmd);
+		switch (cmd)
+		{
+		case "ADD":
+			PhoneBook::add();
+			break;
+		case "SEARCH":
+			PhoneBook::search();
+			break;
+		default:
+			std::cout << "Insert the command!" << std::endl;
+			break;
+		}
+	}
+}
+
 void	PhoneBook::add()
 {
 	this->contacts[this->fixed_index % 8].setContact();
@@ -9,7 +33,14 @@ void	PhoneBook::add()
 
 void	PhoneBook::search()
 {
-	//show all info
-	//wait input
-	//show more info of specific row
+	string	cmd;
+	int	index;
+
+	cmd = "search";
+	while (cmd != "BACK")
+	{
+		//show all
+		std::cin >> cmd;
+		
+	}
 }

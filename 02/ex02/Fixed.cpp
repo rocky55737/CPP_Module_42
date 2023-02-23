3196b782d4af7d64e0ecd7de58f31d7a00365a4b
rocky55737 <rocky55737@gmail.com>
2023-02-24 02:24:11 +0900

    02 ex02 create

---
+++ 02/ex02/Fixed.cpp
@@ -0,0 +1,120 @@
+#include "Fixed.hpp"
+# include <cmath>
+
+Fixed::Fixed() : num(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->num = copy.getRawBits();
+	return *this;
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	return (this->num);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->num = raw;
+}
+
+Fixed::Fixed(int num_init) : num(num_init * 256)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float num_init) : num(roundf(num_init * 256))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+int Fixed::toInt() const
+{
+	return ((int)(this->num / 256));
+}
+
+float Fixed::toFloat() const
+{
+	return ((float)this->num / 256);
+}
+
+std::ostream& operator<<(std::ostream& out_stream, const Fixed& fixed)
+{
+	return (out_stream << fixed.toFloat());
+}
+
+bool Fixed::operator>(const Fixed& copy) const
+{
+	return (this->num > copy.num);
+}
+
+bool Fixed::operator<(const Fixed& copy) const
+{
+	return (this->num < copy.num);
+}
+
+bool Fixed::operator>=(const Fixed& copy) const
+{
+	return (this->num >= copy.num);
+}
+
+bool Fixed::operator<=(const Fixed& copy) const
+{
+	return (this->num <= copy.num);
+}
+
+bool Fixed::operator==(const Fixed& copy) const
+{
+	return (this->num == copy.num);
+}
+
+bool Fixed::operator!=(const Fixed& copy) const
+{
+	return (this->num != copy.num);
+}
+
+Fixed Fixed::operator+(const Fixed& copy) const
+{
+	Fixed temp(this->num + copy.num);
+	return (temp);
+}
+
+Fixed Fixed::operator-(const Fixed& copy) const
+{
+	Fixed temp(this->num - copy.num);
+	return (temp);
+}
+
+Fixed Fixed::operator*(const Fixed& copy) const
+{
+	Fixed temp(this->num * copy.num / 256);
+	return (temp);
+}
+
+Fixed Fixed::operator/(const Fixed& copy) const
+{
+	Fixed temp(this->num * 256 / copy.num);
+	return (temp);
+}
+
+Fixed& Fixed::operator++(void)
+{
+	this->num++;
+	return (*this);
+}

36d7a03f73b640ac941e6fdd14846c470837e06d
RHONG <rocky55737@gmail.com>
2023-03-02 14:56:06 +0900

    04 ex00 Animal class

---
+++ 04/ex00/Animal.cpp
@@ -0,0 +1,24 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("None")
+{
+	std::cout << "Animal deafult constructor called." << std::endl;
+}
+
+Animal::Animal(const Animal& copy)
+{
+	*this = copy;
+	std::cout << "Animal copy constructor called." << std::endl;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called." << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& copy)
+{
+	this->type = copy.type;
+	std::cout << "Animal assignment operator called." << std::endl;
+	return (*this);
+}

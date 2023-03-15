1e5a452cfe5c208608ad7980265c1bd7405d33b7
RHONG <rocky55737@gmail.com>
2023-03-15 13:22:40 +0900

    04 leak checked

--- 04/ex01/main.cpp
+++ 04/ex01/main.cpp
@@ -1,34 +1,23 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include <stdlib.h>
-
-void leaks(void)
-{
-	system("leaks a.out");
-}
 
 int main()
 {
-	atexit(leaks);
-	// const Animal* j = new Dog();
-	// const Animal* i = new Cat();
-	// delete j;//should not create a leak
-	// delete i;
-	
-	// Animal *ani_arr[4];
-	// for (int i = 0; i < 4; i++)
-	// {
-	// 	if (i < 2)
-	// 		ani_arr[i] = new Dog();
-	// 	else
-	// 		ani_arr[i] = new Cat();
-	// }
-	// for (int i = 0; i < 4; i++)
-	//  	delete ani_arr[i];
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak
+	delete i;
 
-	const Cat i;
-	const Cat j = i;
-	const Cat k(j);
+	Animal *ani_arr[4];
+	for (int i = 0; i < 4; i++)
+	{
+		if (i < 2)
+			ani_arr[i] = new Dog();
+		else
+			ani_arr[i] = new Cat();
+	}
+	for (int i = 0; i < 4; i++)
+	 	delete ani_arr[i];
 
 	return (0);
 }

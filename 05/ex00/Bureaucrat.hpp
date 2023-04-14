de96348762dcc18e88da06062b323e4ae8070bcb
rocky55737 <rocky55737@gmail.com>
2023-04-14 19:15:55 +0900

    header without exception class

--- 05/ex00/Bureaucrat.hpp
+++ 05/ex00/Bureaucrat.hpp
@@ -13,4 +13,28 @@
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <iostream>
+
+# define GRADE_HIGHST	1
+# define GRADE_LOWEST	150
+# define GRADE_DEAFALT	GRADE_LOWEST
+
+class Bureaucrat 
+{
+private:
+	const std::string	name;
+	int	grade;
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string& name, int grade);
+	Bureaucrat(const Bureaucrat& copy);
+	Bureaucrat& operator=(const Bureaucrat& copy);
+	~Bureaucrat();
+	const std::string	getName() const;
+	int	getGrade() const;
+	void	setGrade(const int grade); 
+	int	incrementGrade();
+	int	decrementGrade();
+}
+
 #endif

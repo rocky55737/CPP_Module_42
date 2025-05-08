aed383a885ff776273cc2d7c158432ee8e603975
Rokgi Hong <rhong@c1r4s3.42seoul.kr>
2025-05-08 12:17:32 +0900

    Intern 0

--- 05/ex03/Intern.cpp
+++ 05/ex03/Intern.cpp
@@ -6,11 +6,76 @@
 /*   By: rhong <rhong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 18:52:51 by rhong             #+#    #+#             */
-/*   Updated: 2025/05/07 18:55:29 by rhong            ###   ########.fr       */
+/*   Updated: 2025/05/08 12:17:07 by rhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Intern.hpp"
 #include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
-AForm* Intern::makeForm(const std::string& form_name, const std::string& form_target);
+Intern::Intern()
+{
+    std::cout << "Intern default constructor called." << std::endl;
+    std::cout << std::endl;
+}
+
+Intern::~Intern()
+{
+    std::cout << "Intern default destructor called." << std::endl;
+    std::cout << std::endl;
+}
+
+Intern::Intern(const Intern& copy)
+{
+    *this = copy;
+	std::cout << "Intern copy constructor called." << std::endl;
+	std::cout << std::endl;
+}
+
+Intern& Intern::operator=(const Intern& copy)
+{
+    (void)copy;
+    std::cout << "Intern assignment called." << std::endl;
+	std::cout << std::endl;
+}
+
+static AForm* createShrubbery(const std::string& target) {
+	return new ShrubberyCreationForm(target);
+}
+static AForm* createRobotomy(const std::string& target) {
+	return new RobotomyRequestForm(target);
+}
+static AForm* createPresidential(const std::string& target) {
+	return new PresidentialPardonForm(target);
+}
+
+AForm* Intern::makeForm(const std::string& form_name, const std::string& form_target)
+{
+    std::string form_names[3] = {
+		"shrubbery creation",
+		"robotomy request",
+		"presidential pardon"
+	};
+
+	AForm* (*form_creators[3])(const std::string&) = {
+		&createShrubbery,
+		&createRobotomy,
+		&createPresidential
+	};
+
+	for (int i = 0; i < 3; ++i)
+	{
+		if (form_name == form_names[i])
+		{
+			std::cout << "Intern creates " << form_name << std::endl;
+			return form_creators[i](form_target);
+		}
+	}
+
+	
+	return NULL;
+}
--- 05/ex03/Intern.hpp
+++ 05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: rhong <rhong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 18:34:28 by rhong             #+#    #+#             */
-/*   Updated: 2025/05/07 18:54:38 by rhong            ###   ########.fr       */
+/*   Updated: 2025/05/08 12:13:58 by rhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,20 @@
 class Intern
 {
 public:
+    Intern();
+    ~Intern();
+    Intern(const Intern& copy);
+    Intern& operator=(const Intern& copy);
     AForm* makeForm(const std::string& form_name, const std::string& form_target);
-    
+    class FormNotFoundException : public std::exception
+    {
+        private:
+			std::string error_message;
+		public:
+			const char* what() const throw();
+			FormNotFoundException(const std::string& err_bureaucrat_name);
+			virtual ~FormNotFoundException() throw(){};
+    };
 };
 
 #endif
